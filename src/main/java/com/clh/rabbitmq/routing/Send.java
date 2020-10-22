41b1b430d626885d4804384068acbf8430e1e1d0
cat-boy <13510075819@163.com>
2020-10-22 10:07:19 +0800

    上传rabbitmqTest示例代码

---
+++ src/main/java/com/clh/rabbitmq/routing/Send.java
@@ -0,0 +1,30 @@
+package com.clh.rabbitmq.routing;
+
+import com.clh.rabbitmq.util.ConnectionUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Send {
+
+    private static final String EXCHANGE_NAME ="test_exchange_direct";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME,"direct");
+
+        String msg = "hello direct!";
+
+        String routingKey = "info";
+        channel.basicPublish(EXCHANGE_NAME,routingKey,null,msg.getBytes());
+
+        System.out.println("send" + msg);
+        channel.close();
+        connection.close();
+
+
+    }
+}
